738a73642a23b67c3c6fbaae9748cf328cf41e37
unknown <qairunniza@gmail.com>
2024-11-28 23:28:37 +0800

    Assignment1

--- Assignment1.java
+++ Assignment1.java
@@ -14,15 +14,17 @@ public class Assignment1 {
     }
 
     public void displayDetails() {
+        double value = (price * manufactureYear) / 1000;
         System.out.println("Car Name: " + name);
         System.out.println("Manufacture Year: " + manufactureYear);
         System.out.println("Price: " + price);
+        System.err.println("Value: " + value);
+
     }
 
     public static void main(String[] args) {
-        // Create a car object
         Assignment1 car = new Assignment1(2002, 50000, "Myvi");
         car.displayDetails();
-        System.out.println("Calculated Value (Price * Year): " + car.calculateValue());
+        
     }
 }
